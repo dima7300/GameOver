017d51f3c15f8524c684f1144c32e3ee64b2059f
dimapetrov <zuma0000006@gmail.com>
2022-08-29 21:00:48 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+public class Game {
+    int muss[];
+    public int startGame() {
+
+        for (int i = 0; i < 9; i++){
+            muss[i] = i;
+
+        }
+
+            return startGame();
+    }
+    public int drawCanvas(){
+
+        return drawCanvas();
+    }
+}
